a4fd3bb02da40c0a4ccf766b0a4c0cbfd426043c
yohannes11 <johannes.neg@gmail.com>
2026-04-05 15:28:08 +0300

    updates the requirments.txt file and updates the settings file

--- ethiopicdaily/settings.py
+++ ethiopicdaily/settings.py
@@ -84,12 +84,20 @@ WSGI_APPLICATION = 'ethiopicdaily.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+_database_url = os.environ.get('DATABASE_URL')
+if _database_url:
+    DATABASES = {
+        'default': dj_database_url.parse(_database_url, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
